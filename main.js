5e3f5da6a28cf717c783f19cb9afe83e45da5ba5
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-02-08 16:49:03 -0300

    finzalizado

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const main = document.querySelector("main");
+const main = document.querySelector('main[data-theme="dark"]');
+console.log(main);
 const root = document.querySelector(":root");
 const input = document.getElementById("input");
 const resultInput = document.getElementById("result");
@@ -56,6 +57,26 @@ const equal = document.getElementById("equal");
 equal.addEventListener("click", calculate);
 
 function calculate() {
+  resultInput.value = "ERROR";
+  resultInput.classList.add("error");
   const result = eval(input.value);
   resultInput.value = result;
+  resultInput.classList.remove("error");
 }
+
+const themeSwitcher = document.getElementById("themeSwitcher");
+themeSwitcher.addEventListener("click", () => {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9");
+    root.style.setProperty("--border-color", "#aaa");
+    root.style.setProperty("--font-color", "#212529");
+    root.style.setProperty("--primary-color", "#26834a");
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--bg-color", "#212529");
+    root.style.setProperty("--border-color", "#666");
+    root.style.setProperty("--font-color", "#f1f5f9");
+    root.style.setProperty("--primary-color", "#4dff91");
+    main.dataset.theme = "dark";
+  }
+});
